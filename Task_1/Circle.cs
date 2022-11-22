c4053258ec00d612ea2220da11f2592f05638f80
Андрей Ларин <andrewlarin17@gmail.com>
2022-11-22 21:48:01 +0300

    Добавьте файлы проекта.

---
+++ Task_1/Circle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    static class Circle
+    {
+        static public double GetCircleLength(double r)
+        {
+            return 2 * Math.PI * r;
+        }
+        static public double GetCircleSquare(double r)
+        {
+            return r * r * Math.PI;
+        }
+        static public string GetCircleDot(double x, double y, double r)
+        {
+            double triangle = Math.Sqrt(x * x + y * y);
+                if (triangle <= r)
+                return "Точка в круге";
+                else 
+                return "Точка вне круга";
+        }
+    }
+}
---
+++ Task_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double r = Convert.ToDouble(Console.ReadLine());
+            double length = Circle.GetCircleLength(r);
+            double square = Circle.GetCircleSquare(r);
+            Console.WriteLine($"{length} \n{square}");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y = Convert.ToDouble(Console.ReadLine());
+            string dot = Circle.GetCircleDot(r, x, y);
+            Console.WriteLine($"{dot}");
+            Console.ReadKey();
+        }
+    }
+}
